870837f1bda7f0dc996ad8085464c88a6cc70ae6
unknown <G00425673@atu.ie>
2023-02-06 19:05:20 +0000

    week2 - labs

---
+++ week2/nameAndAge.py
@@ -0,0 +1,10 @@
+# Author : Andreia Santos
+# Programe name: nameAndAge
+# Program description : This program says Hello to the user and says its age. Both information are inserted by the user
+
+
+name=input("Please insert your name: ")
+
+age=int(input("Please insert your age: "))
+
+print(f"\n\nHello {name}, \tyour age is {age}")
